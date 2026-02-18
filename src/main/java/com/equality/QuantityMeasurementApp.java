c3533c2f003116f0a0227805b0bc2e2ee2413734
Lakshya Varshney <lakshyavarshney316@gmail.com>
2026-02-18 21:42:19 +0530

    [Lakshya]Add.Added UC2 - FeetAndInchesMeasurementEquality

--- src/main/java/com/equality/QuantityMeasurementApp.java
+++ src/main/java/com/equality/QuantityMeasurementApp.java
@@ -1,19 +1,17 @@
 package com.equality;
-public class QuantityMeasurementApp 
-{
+public class QuantityMeasurementApp {
     public static class Feet {
         private final double value;
         public Feet(double value) {
             this.value = value;
         }
+        public double getValue() {
+            return value;
+        }
         @Override
         public boolean equals(Object obj) {
-            if (this == obj) {
-                return true;
-            }   
-            if (obj == null || getClass() != obj.getClass()) {
-                return false;
-            }
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
             Feet other = (Feet) obj;
             return Double.compare(this.value, other.value) == 0;
         }
@@ -22,13 +20,45 @@ public class QuantityMeasurementApp
             return Double.hashCode(value);
         }
     }
+    public static class Inches {
+        private final double value;
+        public Inches(double value) {
+            this.value = value;
+        }
+        public double getValue() {
+            return value;
+        }
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+            Inches other = (Inches) obj;
+            return Double.compare(this.value, other.value) == 0;
+        }
+        @Override
+        public int hashCode() {
+            return Double.hashCode(value);
+        }
+    }
+    public static boolean compareFeet(double value1, double value2) {
+        Feet feet1 = new Feet(value1);
+        Feet feet2 = new Feet(value2);
+        return feet1.equals(feet2);
+    }
+    public static boolean compareInches(double value1, double value2) {
+        Inches inch1 = new Inches(value1);
+        Inches inch2 = new Inches(value2);
+        return inch1.equals(inch2);
+    }
     public static void main(String[] args) {
-        Feet feet1 = new Feet(1.0);
-        Feet feet2 = new Feet(1.0);
-        Feet feet3 = new Feet(2.0);
-        System.out.println("Comparing 1.0 ft and 1.0 ft: " + feet1.equals(feet2));
-        System.out.println("Comparing 1.0 ft and 2.0 ft: " + feet1.equals(feet3));
-        System.out.println("Comparing 1.0 ft with null: " + feet1.equals(null));
-        System.out.println("Comparing 1.0 ft with itself: " + feet1.equals(feet1));
+        double feetValue1 = 1.0;
+        double feetValue2 = 1.0;
+        double inchValue1 = 1.0;
+        double inchValue2 = 1.0;
+        System.out.println("Input: " + feetValue1 + " ft and " + feetValue2 + " ft");
+        System.out.println("Output: Equal (" + compareFeet(feetValue1, feetValue2) + ")");
+        System.out.println();
+        System.out.println("Input: " + inchValue1 + " inch and " + inchValue2 + " inch");
+        System.out.println("Output: Equal (" + compareInches(inchValue1, inchValue2) + ")");
     }
 }
--- src/test/java/com/equality/QuantityMeasurementAppTest.java
+++ src/test/java/com/equality/QuantityMeasurementAppTest.java
@@ -1,89 +1,82 @@
 package com.equality;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 public class QuantityMeasurementAppTest {
     @Test
-    @DisplayName("Given two feet measurements of 1.0, when compared, then should be equal")
-    public void testEquality_SameValue()
-    {
-        QuantityMeasurementApp.Feet feet1 = new QuantityMeasurementApp.Feet(1.0);
-        QuantityMeasurementApp.Feet feet2 = new QuantityMeasurementApp.Feet(1.0);
-        assertEquals(feet1, feet2, "The measurements should be identical");
+    @DisplayName("Feet: Same value should be equal")
+    void testFeetEquality_SameValue() {
+        QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
+        QuantityMeasurementApp.Feet f2 = new QuantityMeasurementApp.Feet(1.0);
+        assertEquals(f1, f2);
     }
     @Test
-    @DisplayName("Given two feet measurements of 1.0 and 2.0, when compared, then should not be equal")
-    public void testEquality_DifferentValue()
-    {
-        QuantityMeasurementApp.Feet feet1 = new QuantityMeasurementApp.Feet(1.0);
-        QuantityMeasurementApp.Feet feet2 = new QuantityMeasurementApp.Feet(2.0);
-        assertNotEquals(feet1, feet2, "1.0 ft should not be equal to 2.0 ft");
+    @DisplayName("Feet: Different values should not be equal")
+    void testFeetEquality_DifferentValue() {
+        QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
+        QuantityMeasurementApp.Feet f2 = new QuantityMeasurementApp.Feet(2.0);
+        assertNotEquals(f1, f2);
     }
     @Test
-    @DisplayName("Given a feet measurement, when compared to null, then should not be equal")
-    public void testEquality_NullComparison()
-    {
-        QuantityMeasurementApp.Feet feet1 = new QuantityMeasurementApp.Feet(1.0);
-        assertNotEquals(feet1, null, "Feet object should not be equal to null");
+    @DisplayName("Feet: Null comparison should not be equal")
+    void testFeetEquality_NullComparison() {
+        QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
+        assertNotEquals(f1, null);
     }
     @Test
-    @DisplayName("Given a feet measurement, when compared to non-Feet object, then should not be equal")
-    public void testEquality_NonNumericInput()
-    {
-        QuantityMeasurementApp.Feet feet1 = new QuantityMeasurementApp.Feet(1.0);
-        String nonNumeric = "1.0";
-        assertNotEquals(feet1, nonNumeric, "Feet object should not be equal to non-Feet object");
+    @DisplayName("Feet: Type safety check")
+    void testFeetEquality_TypeCheck() {
+        QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
+        assertNotEquals(f1, "1.0");
     }
     @Test
-    @DisplayName("Given a feet measurement, when compared to itself, then should be equal")
-    public void testEquality_SameReference()
-    {
-        QuantityMeasurementApp.Feet feet1 = new QuantityMeasurementApp.Feet(1.0);
-        assertEquals(feet1, feet1, "Feet object should be equal to itself (reflexive)");
+    @DisplayName("Feet: Same reference should be equal")
+    void testFeetEquality_SameReference() {
+        QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
+        assertEquals(f1, f1);
     }
     @Test
-    @DisplayName("Given two equal feet measurements, when compared in any order, then should be symmetric")
-    public void testEquality_SymmetricProperty()
-    {
-        QuantityMeasurementApp.Feet feet1 = new QuantityMeasurementApp.Feet(3.0);
-        QuantityMeasurementApp.Feet feet2 = new QuantityMeasurementApp.Feet(3.0);
-        assertEquals(feet1, feet2, "Equality should be symmetric");
-        assertEquals(feet2, feet1, "Equality should be symmetric");
+    @DisplayName("Inches: Same value should be equal")
+    void testInchesEquality_SameValue() {
+        QuantityMeasurementApp.Inches i1 = new QuantityMeasurementApp.Inches(1.0);
+        QuantityMeasurementApp.Inches i2 = new QuantityMeasurementApp.Inches(1.0);
+        assertEquals(i1, i2);
     }
     @Test
-    @DisplayName("Given three equal feet measurements, when compared transitively, then should be equal")
-    public void testEquality_TransitiveProperty()
-    {
-        QuantityMeasurementApp.Feet feet1 = new QuantityMeasurementApp.Feet(5.0);
-        QuantityMeasurementApp.Feet feet2 = new QuantityMeasurementApp.Feet(5.0);
-        QuantityMeasurementApp.Feet feet3 = new QuantityMeasurementApp.Feet(5.0);
-        assertEquals(feet1, feet2, "Equality should be transitive");
-        assertEquals(feet2, feet3, "Equality should be transitive");
-        assertEquals(feet1, feet3, "Equality should be transitive");
+    @DisplayName("Inches: Different values should not be equal")
+    void testInchesEquality_DifferentValue() {
+        QuantityMeasurementApp.Inches i1 = new QuantityMeasurementApp.Inches(1.0);
+        QuantityMeasurementApp.Inches i2 = new QuantityMeasurementApp.Inches(2.0);
+        assertNotEquals(i1, i2);
     }
     @Test
-    @DisplayName("Given two feet measurements of 0.0, when compared, then should be equal")
-    public void testEquality_ZeroValue()
-    {
-        QuantityMeasurementApp.Feet feet1 = new QuantityMeasurementApp.Feet(0.0);
-        QuantityMeasurementApp.Feet feet2 = new QuantityMeasurementApp.Feet(0.0);
-        assertEquals(feet1, feet2, "0.0 ft should be equal to 0.0 ft");
+    @DisplayName("Inches: Null comparison should not be equal")
+    void testInchesEquality_NullComparison() {
+        QuantityMeasurementApp.Inches i1 = new QuantityMeasurementApp.Inches(1.0);
+        assertNotEquals(i1, null);
     }
     @Test
-    @DisplayName("Given two feet measurements of -1.0, when compared, then should be equal")
-    public void testEquality_NegativeValue()
-    {
-        QuantityMeasurementApp.Feet feet1 = new QuantityMeasurementApp.Feet(-1.0);
-        QuantityMeasurementApp.Feet feet2 = new QuantityMeasurementApp.Feet(-1.0);
-        assertEquals(feet1, feet2, "-1.0 ft should be equal to -1.0 ft");
+    @DisplayName("Inches: Type safety check")
+    void testInchesEquality_TypeCheck() {
+        QuantityMeasurementApp.Inches i1 = new QuantityMeasurementApp.Inches(1.0);
+        assertNotEquals(i1, "1.0");
     }
     @Test
-    @DisplayName("Given two feet measurements with decimal values, when compared, then should handle precision")
-    public void testEquality_DecimalPrecision()
-    {
-        QuantityMeasurementApp.Feet feet1 = new QuantityMeasurementApp.Feet(1.5);
-        QuantityMeasurementApp.Feet feet2 = new QuantityMeasurementApp.Feet(1.5);
-        assertEquals(feet1, feet2, "1.5 ft should be equal to 1.5 ft");
+    @DisplayName("Inches: Same reference should be equal")
+    void testInchesEquality_SameReference() {
+        QuantityMeasurementApp.Inches i1 = new QuantityMeasurementApp.Inches(1.0);
+        assertEquals(i1, i1);
+    }
+    @Test
+    @DisplayName("Static method: compareFeet should return true for equal values")
+    void testCompareFeet() {
+        assertTrue(QuantityMeasurementApp.compareFeet(2.0, 2.0));
+        assertFalse(QuantityMeasurementApp.compareFeet(2.0, 3.0));
+    }
+    @Test
+    @DisplayName("Static method: compareInches should return true for equal values")
+    void testCompareInches() {
+        assertTrue(QuantityMeasurementApp.compareInches(5.0, 5.0));
+        assertFalse(QuantityMeasurementApp.compareInches(5.0, 6.0));
     }
 }
