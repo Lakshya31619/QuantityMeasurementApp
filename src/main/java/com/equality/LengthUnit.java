22269e900b45e8db51da2506162a73a793bdf58d
Lakshya Varshney <lakshyavarshney316@gmail.com>
2026-03-09 17:47:54 +0530

    [Lakshya]Add.Added UC-10

--- src/main/java/com/equality/LengthUnit.java
+++ src/main/java/com/equality/LengthUnit.java
@@ -1,20 +1,40 @@
 package com.equality;
-public enum LengthUnit {
-    FEET(1.0),
-    INCH(1.0 / 12.0),
-    YARD(3.0),
-    CENTIMETER((0.393701) / 12.0);
-    private final double toFeetFactor;
-    LengthUnit(double toFeetFactor) {
-        this.toFeetFactor = toFeetFactor;
+
+public enum LengthUnit implements Measurable {
+    FEET(1.0, "FEET"),
+    INCH(1.0 / 12.0, "INCH"),
+    YARD(3.0, "YARD"),
+    CENTIMETER((0.393701) / 12.0, "CENTIMETER");
+
+    private final double conversionFactor;
+    private final String unitName;
+
+    LengthUnit(double conversionFactor, String unitName) {
+        this.conversionFactor = conversionFactor;
+        this.unitName = unitName;
     }
+
+    @Override
+    public double getConversionFactor() {
+        return conversionFactor;
+    }
+
     public double toFeetFactor() {
-        return toFeetFactor;
+        return conversionFactor;
     }
+
+    @Override
     public double convertToBaseUnit(double value) {
-        return value * toFeetFactor;
+        return value * conversionFactor;
     }
+
+    @Override
     public double convertFromBaseUnit(double baseValue) {
-        return baseValue / toFeetFactor;
+        return baseValue / conversionFactor;
+    }
+
+    @Override
+    public String getUnitName() {
+        return unitName;
     }
 }
---
+++ src/main/java/com/equality/Measurable.java
@@ -0,0 +1,7 @@
+package com.equality;
+public interface Measurable {
+    double getConversionFactor();
+    double convertToBaseUnit(double value);
+    double convertFromBaseUnit(double baseValue);
+    String getUnitName();
+}
